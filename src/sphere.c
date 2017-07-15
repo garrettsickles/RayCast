5f4a2ef91d38a166adfe0c6678166dfd295e7562
Garrett Sickles <garrettsickles@gmail.com>
2017-07-15 13:57:02 -0600

    Cleanup sphere

--- src/sphere.c
+++ src/sphere.c
@@ -9,11 +9,7 @@ Vector4 RayIntoSphere(Vector4 origin, Vector4 direction, Vector4 sphere)
 	Vector4 displacement, av, bv, cv, dv, intersection;
 
 	// displacement = origin - sphere 
-	displacement =
-		_mm256_sub_pd
-		(
-			origin, sphere
-		);
+	displacement = _mm256_sub_pd(origin, sphere);
 
 	// Insert radius into unused displacement parameter
 	T(displacement) = T(sphere);
@@ -47,20 +43,11 @@ Vector4 RayIntoSphere(Vector4 origin, Vector4 direction, Vector4 sphere)
 	else
 	{
 		time = Z(dv);
-
-		// Two roots
 		if (discriminant > 0.0)
 		{
-			if (c < 0.0)
-			{
-				time += sqrt(discriminant);
-			}
-			else
-			{
-				time -= sqrt(discriminant);
-			}
+			if (c < 0.0) time += sqrt(discriminant);
+			else         time -= sqrt(discriminant);
 		}
-
 	}
 
 	time = time / T(dv);
